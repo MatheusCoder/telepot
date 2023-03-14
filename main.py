a2823832dcc7495d579e21d7e1e33f5dc943dc23
MatheusCoder <100002554+MatheusCoder@users.noreply.github.com>
2023-03-14 13:51:16 +0000

    1º commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import telepot
+bot = telepot.Bot("6118176276:AAHUIhXINEsywU1cHHhM8WkyYQLPLGLTGno")
+bot.getUpdates()
+bot.sendMessage(2048202268,"Olá Matheus, Bom dia")
+
+#def Mensagens(msg):
+#    print(msg["text"])
+#bot.message_loop(Mensagens)
+#while True:
+#    pass
